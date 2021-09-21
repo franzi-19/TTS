9feaed1d3d9d68499a841243edce114f151c1fb3
franzi-19 <franziska.dieckmann@web.de>
2021-09-21 14:42:56 +0200

    fixed plotting

--- TTS/speaker_encoder/visuals.py
+++ TTS/speaker_encoder/visuals.py
@@ -29,16 +29,22 @@ colormap = (
 )
 
 
-def plot_embeddings(embeddings, num_utter_per_speaker):
-    embeddings = embeddings[: 10 * num_utter_per_speaker]
+def plot_embeddings(embeddings, num_utter_per_speaker, labels, max_speaker=10, max_utter=10): # [500, 256]
+    embeddings = embeddings.reshape(embeddings.shape[0] // num_utter_per_speaker, num_utter_per_speaker, -1)
+    num_utter_per_speaker = min(num_utter_per_speaker, max_utter)
+    embeddings = embeddings[: max_speaker, : max_utter].reshape(-1, embeddings.shape[-1])
+    # embeddings = embeddings[: max_speaker * num_utter_per_speaker] # [100, 256]
     model = umap.UMAP()
-    projection = model.fit_transform(embeddings)
+    projection = model.fit_transform(embeddings) #[x, 2]
     num_speakers = embeddings.shape[0] // num_utter_per_speaker
-    ground_truth = np.repeat(np.arange(num_speakers), num_utter_per_speaker)
+    print(f'will plot {num_speakers} speakers with {num_utter_per_speaker} utterances each')
+    ground_truth = np.repeat(np.arange(num_speakers), num_utter_per_speaker) # [100], [0,0,0,...,1,1,1,...]
     colors = [colormap[i] for i in ground_truth]
 
     fig, ax = plt.subplots(figsize=(16, 10))
-    _ = ax.scatter(projection[:, 0], projection[:, 1], c=colors)
+    scatter = ax.scatter(projection[:, 0], projection[:, 1], c=colors)
+    x=np.array(labels)[:,0].tolist()
+    ax.legend(labels=np.array(labels)[:,0].tolist(), loc="upper right", title="Classes")  
     plt.gca().set_aspect("equal", "datalim")
     plt.title("UMAP projection")
     plt.tight_layout()
